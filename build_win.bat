3711e5e4869d8102b7139ad8b584595c39979470
Ivan S <ivan@ims-productions.com>
2024-12-26 23:07:17 -0600

    build scripts now use py launcher

--- build_win.bat
+++ build_win.bat
@@ -1,13 +1,11 @@
 @echo off
 SET VENV_DIR=uaaccess_env
 IF NOT EXIST %VENV_DIR% (
-    echo Virtual environment not found. Creating...
-    python -m venv %VENV_DIR%
+echo Virtual environment not found. Creating...
+python -m venv %VENV_DIR%
 )
-
 echo Activating virtual environment...
 call %VENV_DIR%\Scripts\activate
-
 echo Running build script...
 python build_uaaccess.py
 deactivate
