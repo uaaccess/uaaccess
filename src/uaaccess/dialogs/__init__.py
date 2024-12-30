dac9292f51eff8c926ea66298a5e1c56612ae9a4
Ethin Probst <ethindp@pm.me>
2024-12-30 03:42:50 -0600

    Ruff: fix invalid-all-object (PLE0604) error

--- src/uaaccess/dialogs/__init__.py
+++ src/uaaccess/dialogs/__init__.py
@@ -2,4 +2,4 @@ from .effect_parameters_dialog import EffectParametersDialog
 from .preamp_effects_dialog import PreampEffectsDialog
 from .sends_dialog import SendsDialog, SendsType
 
-__all__ = [SendsType, SendsDialog, PreampEffectsDialog, EffectParametersDialog]
+__all__ = ["SendsType", "SendsDialog", "PreampEffectsDialog", "EffectParametersDialog"]
